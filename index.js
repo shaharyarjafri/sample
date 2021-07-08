2959d54288a4f5593bc0947bb9c58921d791cce3
shaharyarjafri <shaharyarjafri@gmail.com>
2021-07-09 03:02:18 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("Hello from my package");
+
+const sayHello = (name) => {
+  console.log(`hello ${name}`);
+};
+
+module.exports = (sayHello)
+
+// console.log("STARTIG     ");
+//   const { exec } = require('child_process');
+// 	return new Promise(function (resolve, reject) {
+// 		exec(
+// 					`cd / && D: && C:/opa eval -d D:/play_example.rego -d play_data.json -i play_input.json "data.app.rbac"`,
+// 					(error, stdout, stderr) => {
+// 					  if (error) {
+// 						reject(error);
+// 						return;
+// 					  }
+// 					  resolve(stdout.trim());
+// 					}
+// 				  );
+// 				});
+
+//       }
