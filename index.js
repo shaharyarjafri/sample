6ef04f9a84806ccabbb9d87080332544a78693fd
shaharyarjafri <shaharyarjafri@gmail.com>
2021-07-19 19:10:32 +0500

    added readme

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
-console.log("Hello from my package");
+async function execPackage(body) {
+  result = await execOPA(body);
+  return result;
+}
+async function execOPA(body) {
+  var https = require("http");
+  return new Promise(async function (resolve, reject) {
+    const { StringDecoder } = require('string_decoder');
+    jsonObject = JSON.stringify(body);
+    var postheaders = {
+      "Content-Type": "application/json",
+      "Content-Length": Buffer.byteLength(jsonObject, "utf8"),
+    };
+    var optionspost = {
+      host: "localhost",
+      port: 3331,
+      path: "/api/v1/daml/checkAuthentication",
+      method: "POST",
+      headers: postheaders,
+    };
+    var reqPost = await https.request(optionspost, async function (res) {
+      console.log("statusCode: ", res.statusCode);
+      var decoder = new StringDecoder("utf-8");
+      var buffer = "";
 
-const sayHello = (name) => {
-  console.log(`hello ${name}`);
-};
+      await res.on("data", (d) => {
+        console.info("POST result:\n");
+        resolve(
+          (buffer += decoder.write(d))
+          // process.stdout.write(d)
+        );
+        console.info("\n\nPOST completed");
+        return buffer;
+      });
+    });
 
-module.exports = (sayHello)
+    reqPost.write(jsonObject);
+    reqPost.end();
+    reqPost.on("error", function (e) {
+      console.error("error inside  ", e);
+    });
+  });
+}
 
-// console.log("STARTIG     ");
-//   const { exec } = require('child_process');
-// 	return new Promise(function (resolve, reject) {
-// 		exec(
-// 					`cd / && D: && C:/opa eval -d D:/play_example.rego -d play_data.json -i play_input.json "data.app.rbac"`,
-// 					(error, stdout, stderr) => {
-// 					  if (error) {
-// 						reject(error);
-// 						return;
-// 					  }
-// 					  resolve(stdout.trim());
-// 					}
-// 				  );
-// 				});
-
-//       }
+module.exports = execPackage;
